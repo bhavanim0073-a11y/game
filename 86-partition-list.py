f43f79b277e8b26e7ff0e6104729f6ad87eddbbd
bhavanim0073-a11y <bhavanim0073@gmail.com>
2026-09-22 00:19:11 +0530

    Add solution for LeetCode 86 Partition List

---
+++ 86-partition-list.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def partition(self, head, x):
+        less = ListNode(0)
+        greater = ListNode(0)
+
+        less_tail = less
+        greater_tail = greater
+
+        current = head
+
+        while current:
+            if current.val < x:
+                less_tail.next = current
+                less_tail = less_tail.next
+            else:
+                greater_tail.next = current
+                greater_tail = greater_tail.next
+
+            current = current.next
+
+        greater_tail.next = None
+        less_tail.next = greater.next
+
+        return less.next
